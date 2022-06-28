8508150adfa4cfbd1d72332b8363a93e4349f65a
h110401 <hung110401@gmail.com>
2022-06-28 11:40:27 +0700

    fix

--- cal.js
+++ cal.js
@@ -1,3 +1,4 @@
+
 function add(c) {
     document.getElementById('display').value += c;
 }
@@ -9,3 +10,21 @@ function answer() {
 function reset() {
     document.getElementById('display').value = null;
 }
+
+window.addEventListener('keypress', function (e) {
+    if(e.keyCode > 47 && e.keyCode < 58){
+        add(e.key);
+    }
+}, false);
+
+window.addEventListener('keydown', function (e) {
+    if(e.keyCode == 46){
+        reset();
+    } else if (e.key == '/' || e.keyCode == 111 || e.keyCode == 190 || e.keyCode == 107 || e.key == '+' || e.keyCode == 109 || e.key == '-' || e.keyCode == 106 || e.key == '*') {
+        add(e.key)
+    } else if (e.keyCode == 13) {
+        answer()
+    }
+}, false);
+
+
