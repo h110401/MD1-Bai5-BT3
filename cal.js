98539dbd803614af3420ee11fa9fc4393bc14b74
h110401 <hung110401@gmail.com>
2022-06-29 07:52:52 +0700

    fix

--- cal.js
+++ cal.js
@@ -4,27 +4,31 @@ function add(c) {
 }
 function answer() {
     let operation = document.getElementById('display');
-    let result = eval(operation.value);
-    operation.value = result;
+    if(operation.value){
+        let result = eval(operation.value);
+        operation.value = result;
+    }
 }
+
 function reset() {
     document.getElementById('display').value = null;
 }
 
-window.addEventListener('keypress', function (e) {
+document.addEventListener('keypress', function (e) {
     if(e.keyCode > 47 && e.keyCode < 58){
         add(e.key);
     }
 }, false);
 
-window.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function (e) {
     if(e.keyCode == 46){
         reset();
     } else if (e.key == '/' || e.keyCode == 111 || e.keyCode == 190 || e.keyCode == 107 || e.key == '+' || e.keyCode == 109 || e.key == '-' || e.keyCode == 106 || e.key == '*') {
         add(e.key)
     } else if (e.keyCode == 13) {
+
         answer()
+
     }
 }, false);
-
 
